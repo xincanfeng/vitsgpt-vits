661d94ecd372bc08f4037bbb8b8667adb1a44e01
xincan <cecilia.fung@foxmail.com>
2024-04-14 19:36:59 +0000

    u

--- monotonic_align/preprocess_own_data.sh
+++ monotonic_align/preprocess_own_data.sh
@@ -2,7 +2,7 @@
 cd /data/vitsGPT/vits/monotonic_align
 python /data/vitsGPT/vits/monotonic_align/setup.py build_ext --inplace
 
-# Preprocessing (g2p) for your own datasets. Preprocessed phonemes for LJ Speech and VCTK have been already provided.b
+# Preprocessing (g2p) for your own datasets. Preprocessed phonemes for LJSpeech and EmoV_DB have been already provided.
 # python /data/vitsGPT/vits/preprocess.py --text_index 1 --filelists /data/vitsGPT/vits/filelists/ljs_audio_text_train_filelist.txt /data/vitsGPT/vits/filelists/ljs_audio_text_val_filelist.txt /data/vitsGPT/vits/filelists/ljs_audio_text_test_filelist.txt 
 # python /data/vitsGPT/vits/preprocess.py --text_index 2 --filelists /data/vitsGPT/vits/filelists/vctk_audio_sid_text_train_filelist.txt /data/vitsGPT/vits/filelists/vctk_audio_sid_text_val_filelist.txt /data/vitsGPT/vits/filelists/vctk_audio_sid_text_test_filelist.txt
 # python /data/vitsGPT/vits/preprocess.py --text_index 1 --filelists /data/vitsGPT/vits/filelists/jsut_audio_text_train_filelist.txt /data/vitsGPT/vits/filelists/jsut_audio_text_val_filelist.txt /data/vitsGPT/vits/filelists/jsut_audio_text_test_filelist.txt 

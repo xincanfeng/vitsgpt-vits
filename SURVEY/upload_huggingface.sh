814fdba6e675aae8dabd582eed1f7fbfc79a00af
Xincan Feng <83276663+xincanfeng@users.noreply.github.com>
2024-05-01 18:08:25 +0900

    Create upload_huggingface.sh

---
+++ SURVEY/upload_huggingface.sh
@@ -0,0 +1,3 @@
+git clone https://xincan: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@huggingface.co/datasets/xincan/Llama-VITS_data
+git remote set-url origin https://xincan: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@huggingface.co/datasets/xincan/Llama-VITS_data
+git pull origin
